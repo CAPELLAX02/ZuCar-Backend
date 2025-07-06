531f71dd7f15b980ddf633cf7fe0cb576e5a5a28
CAPELLAX02 <ahmtatar@gmail.com>
2025-07-06 23:55:06 +0300

    Fix the authentication routes: remove unused password recovery endpoint routing.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const { authenticateToken } = require('../middlewares/authMiddleware');
 
 router.post('/login', authController.login);
-router.post('/change-password', authenticateToken, authController.changePassword);
 
 module.exports = router;
